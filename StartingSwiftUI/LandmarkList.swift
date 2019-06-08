ed6a34f7fa479fe4b139509f1340c86ff5072f63
Adriano Leal Ramos <ramos.adrianoleal@gmail.com>
2019-06-08 13:04:25 -0300

    SwiftUI Tutorial

---
+++ StartingSwiftUI/LandmarkList.swift
@@ -0,0 +1,46 @@
+//
+//  LandmarkList.swift
+//  StartingSwiftUI
+//
+//  Created by Adriano Ramos on 06/06/19.
+//  Copyright © 2019 Adriano Ramos. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject private var userData: UserData
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $userData.showFavoritesOnly) {
+                    Text("Show Favorites Only")
+                }
+                
+                ForEach(userData.landmarks) { landmark in
+                    if !self.userData.showFavoritesOnly || landmark.isFavorite {
+                        NavigationButton(
+                        destination: LandmarkDetail(landmark: landmark)) {
+                            LandmarkRow(landmark: landmark)
+                        }
+                    }
+                }
+                }
+                .navigationBarTitle(Text("Landmarks"), displayMode: .large)
+        }
+    }
+}
+
+#if DEBUG
+struct LandmarksList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE", "iPhone XS Max"].identified(by: \.self)) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+            }
+            .environmentObject(UserData())
+    }
+}
+#endif
---
+++ StartingSwiftUI/Supporting Views/CircleImage.swift
@@ -0,0 +1,28 @@
+//
+//  CircleImage.swift
+//  StartingSwiftUI
+//
+//  Created by Adriano Ramos on 06/06/19.
+//  Copyright © 2019 Adriano Ramos. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var image: Image
+    
+    var body: some View {
+        image
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+#if DEBUG
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage(image: Image("turtlerock"))
+    }
+}
+#endif
